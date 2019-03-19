aa0ebe10cdabddecee2424c5002d5159abc30af0
Mark Smith <msmith77@nd.edu>
2019-03-19 16:17:31 -0700

    Added summary wrapper tests

---
+++ tests/testthat/test.summary.R
@@ -0,0 +1,26 @@
+context('summary() wrapper Tests')
+
+test_that('commented vectors summarize correctly', {
+  df <- c(1,2,3,4,5,6)
+  df <- notes(df)
+  expect_equal(capture_output_lines(summary(df))[1],"#    Comments                                                       Time Stamp")
+})
+
+test_that('commented lists summarize correctly', {
+  df <- list(1,2,3,4,'a','b')
+  df <- notes(df)
+  expect_equal(capture_output_lines(summary(df))[1],"#    Comments                                                       Time Stamp")
+})
+
+test_that('commented data frames summarize correctly', {
+  df <- (cars)
+  df <- notes(df[1,])
+  expect_equal(capture_output_lines(summary(df))[1],"#    Comments                                                       Time Stamp")
+})
+
+
+test_that('uncommented objects summarize correctly', {
+  df <- cars[1,]
+  expect_equal(capture_output(summary(df)),"")
+})
+
